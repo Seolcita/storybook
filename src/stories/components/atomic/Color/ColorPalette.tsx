26b8c608c02456db5766ced3126283e24b45e1a5
Seolcita <seolcita@gmail.com>
2023-10-12 13:51:17 -0600

    updated to box

--- src/stories/components/atomic/Color/ColorPalette.tsx
+++ src/stories/components/atomic/Color/ColorPalette.tsx
@@ -1,6 +1,7 @@
 import { type ReactElement } from 'react';
 
 import Typography from '../Typography';
+import { Box } from '@mui/material';
 
 interface ColorPaletteProps {
   colorList: [];
@@ -8,38 +9,29 @@ interface ColorPaletteProps {
 
 const ColorPalette = ({ colorList }: ColorPaletteProps): ReactElement => {
   return (
-    <div
-      style={{
-        display: 'flex',
-        flexWrap: 'wrap',
-      }}
-    >
+    <Box display='flex' flexWrap='wrap'>
       {colorList.map((color) => (
-        <div
-          style={{
-            width: '150px',
-            height: '150px',
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'start',
-            alignItems: 'center',
-          }}
+        <Box
+          width='150px'
+          height='150px'
+          display='flex'
+          flexDirection='column'
+          justifyContent='start'
+          alignItems='center'
         >
-          <div
-            style={{
-              borderRadius: '20%',
-              height: '50px',
-              width: '50px',
-              backgroundColor: Object.values(color),
-            }}
-          ></div>
-          <div style={{ textAlign: 'center' }}>
+          <Box
+            borderRadius='20%'
+            height='50px'
+            width='50px'
+            bgcolor={Object.values(color)}
+          ></Box>
+          <Box textAlign='center'>
             <Typography variant='textXS'>{Object.keys(color)}</Typography>
             <Typography variant='textXS'>{Object.values(color)}</Typography>
-          </div>
-        </div>
+          </Box>
+        </Box>
       ))}
-    </div>
+    </Box>
   );
 };
 
